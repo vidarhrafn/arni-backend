0c213828fe45d45d247a341b2bc2039e1f4b2755
vidarhrafn <vidarhrafn@gmail.com>
2025-01-30 00:17:54 +0000

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -32,3 +32,4 @@ export default async function handler(req, res) {
         return res.status(500).json({ error: "Villa við API-fyrirspurn" });
     }
 }
+console.log("Bakendinn var uppfærður:", new Date().toISOString());
